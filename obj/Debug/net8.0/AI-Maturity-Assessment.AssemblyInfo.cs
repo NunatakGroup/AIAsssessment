de349d1899fbd0838541afebcfb45541814c8c3d
Paolo Caesar <paolo.caesar@nunatakmba20-22.local>
2025-02-02 20:37:52 +0100

    Results Page Design Improvements

--- obj/Debug/net8.0/AI-Maturity-Assessment.AssemblyInfo.cs
+++ obj/Debug/net8.0/AI-Maturity-Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45752047800ae8a38a8d309c035d276ed8732025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9746041a7f72e270d79f2427013dd73946adeec")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
