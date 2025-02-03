8e7d95f18d91afc702579f25b3a78e9aa4b92b34
Paolo Caesar <paolo.caesar@nunatakmba20-22.local>
2025-02-03 13:09:24 +0100

    Assessment Page Mobile Design Improvements

--- obj/Debug/net8.0/AI-Maturity-Assessment.AssemblyInfo.cs
+++ obj/Debug/net8.0/AI-Maturity-Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9746041a7f72e270d79f2427013dd73946adeec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de349d1899fbd0838541afebcfb45541814c8c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
