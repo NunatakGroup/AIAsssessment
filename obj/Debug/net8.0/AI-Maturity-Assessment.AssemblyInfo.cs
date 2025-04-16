21b24e6204ecc0b28c15817a6705f7a761f0ae6c
nunapc <paolo.caesar@nunatak.com>
2025-04-16 09:17:04 +0200

    Chapter Color and 3 Nunatak Contacts

--- obj/Debug/net8.0/AI-Maturity-Assessment.AssemblyInfo.cs
+++ obj/Debug/net8.0/AI-Maturity-Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf8f3eea601a3f2a7c767c514d38f7d4cce320f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b68cce5386059e30230a5549ae3f62fbd949e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI-Maturity-Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
